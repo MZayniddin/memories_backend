eb38af8fc7c93b53a96ebcdb7ec115977f13d6e0
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-05-06 14:42:03 +0500

    added update post route

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
     "mongodb+srv://mz:4321@cluster0.mfwoq9t.mongodb.net/MemoriesDB?retryWrites=true&w=majority";
