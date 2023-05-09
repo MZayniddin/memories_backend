995dae973a1f1ea609b6e9cc3a4da79e84728246
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-05-09 14:36:49 +0500

    added sign in and sign up apis

--- controllers/user.js
+++ controllers/user.js
@@ -5,28 +5,28 @@ import User from "../models/user.js";
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
-
+    console.log("REQUESTED")
     try {
         const existingUser = await User.find({ email });
 
-        if (!existingUser)
+        if (!existingUser.length)
             return res.status(404).json({ message: "User doesn't exist" });
 
         const isPasswordCorrect = await bcrypt.compare(
             password,
-            existingUser.password
+            existingUser[0].password
         );
 
         if (!isPasswordCorrect)
             return res.status(400).json({ message: "Invalid credentials" });
 
         const token = jwt.sign(
-            { email: existingUser.email, id: existingUser._id },
+            { email: existingUser[0].email, id: existingUser[0]._id },
             "test",
             { expiresIn: "1h" }
         );
 
-        res.status(200).json({ result: existingUser, token });
+        res.status(200).json({ result: existingUser[0], token });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -37,7 +37,8 @@ export const signup = async (req, res) => {
 
     try {
         const existingUser = await User.find({ email });
-        if (existingUser)
+
+        if (existingUser.length)
             return res.status(400).json({ message: "User already exists" });
 
         if (password !== confirmPassword)
